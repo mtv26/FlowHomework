ec2ff092b15d64bdbae85f17c2be4b318274bb52
Alexey Matveev <m.alex@mail.communigate.ru>
2025-05-12 18:31:42 +0300

    Перейти с коллбеков на саспенд функции и корутины

--- flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
+++ flowcats/src/main/java/otus/homework/flowcats/CatsViewModel.kt
@@ -2,22 +2,44 @@ package otus.homework.flowcats
 
 import androidx.lifecycle.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import otus.homework.flowcats.Result
 
 class CatsViewModel(
     private val catsRepository: CatsRepository
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Fact>()
-    val catsLiveData: LiveData<Fact> = _catsLiveData
+    private val _cats = MutableStateFlow<Result>(
+        Result.Success(
+            Fact(
+                "",
+                false,
+                "",
+                "",
+                "",
+                false,
+                "",
+                "",
+                ""
+            )
+        )
+    )
+    val cats = _cats.asStateFlow()
 
     init {
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
-                catsRepository.listenForCatFacts().collect {
-                    _catsLiveData.value = it
+                catsRepository.listenForCatFacts()
+                    .catch {
+                        _cats.value = Result.Error
+                    }
+                    .collect {
+                        _cats.value = Result.Success(it)
                 }
             }
         }
